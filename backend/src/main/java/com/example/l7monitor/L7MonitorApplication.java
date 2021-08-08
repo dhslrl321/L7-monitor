52dbb2ad98142be18c7c820054aa03e83d212c02
dhslrl321 <dhslrl321@gmail.com>
2021-08-08 16:13:25 +0900

    Add backend/ 백엔드 프로젝트 생성

---
+++ backend/src/main/java/com/example/l7monitor/L7MonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.l7monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L7MonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L7MonitorApplication.class, args);
+    }
+
+}
