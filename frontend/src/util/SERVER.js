aad148c25b8055976ac5c925517f1bade33950be
dhslrl321 <dhslrl321@gmail.com>
2021-08-19 14:13:39 +0900

    #fix XHR 요청 서버 ip 변경

--- frontend/src/util/SERVER.js
+++ frontend/src/util/SERVER.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const SERVER = axios.create({
-  baseURL: "http://back-container:8080/api",
+  baseURL: "http://localhost:8080/api",
   headers: {
     "Content-Type": "application/json",
   }
