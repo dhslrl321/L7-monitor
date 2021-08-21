4cd72ad68b1290bfa7a643ae9795a7574cce3f19
dhslrl321 <dhslrl321@gmail.com>
2021-08-21 20:08:30 +0900

    #Add OnPremise 다운로드 링크 추가

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # L7-monitor
+
 BOB L7 traffic monitoring project
+
+#### On Premise 다운로드 링크
+
+- [구글 드라이브](https://drive.google.com/file/d/14-GcDNflgdRfGDFXtBuunD1kgktfXj3-/view?usp=sharing)
--- execute-service.sh
+++ execute-service.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-echo "hello, world"
+CONF_HOST_IP=$(awk '/^HOST_IP/{print $3}' boribob.conf)
+DOCKER_HOST_IP=$(awk '/^services:/' docker-compose-test.yml)
+
+DOCKER_COMPOSE_FILE=docker-compose-test.yml
+
+echo CONF_HOST IP : $CONF_HOST_IP
+echo DOCKER_HOST_IP : $DOCKER_HOST_IP
+
+exit 0
