152316b5c91f7447bceb18d04295f423b62e38c1
Kim Jinuk <32464833+NownS@users.noreply.github.com>
2021-08-22 03:33:29 +0900

    Update execute-service.sh

--- execute-service.sh
+++ execute-service.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-CONF_HOST_IP=$(awk '/^HOST_IP/{print $3}' boribob.conf)
-DOCKER_HOST_IP=$(awk '/^services:/' docker-compose-test.yml)
 
-DOCKER_COMPOSE_FILE=docker-compose-test.yml
+docker-compose up -d --build
+cd daemon
+pip3 install -r requirements.txt
+python3 server.py &
 
-echo CONF_HOST IP : $CONF_HOST_IP
-echo DOCKER_HOST_IP : $DOCKER_HOST_IP
-
-exit 0
